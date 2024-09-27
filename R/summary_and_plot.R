78baca124d97f450630c0bcde06fa22eb29020e9
Matteo Perini <perini.mtt@gmail.com>
2024-09-27 17:08:31 -0400

    summary and plot function checked in R

--- R/summary_and_plot.R
+++ R/summary_and_plot.R
@@ -13,19 +13,31 @@
 #' now <- as.Date("1990-10-01")
 #'
 #' # perform nowcasting
-#' predictions <- nowcast(denguedat, "onset_week", "report_week", now = now, method = "optimization", seed = 2495624, iter = 10)
+#' predictions <- nowcast(denguedat, "onset_week", "report_week", now = now, method = "optimization",
+#' seed = 2495624, iter = 10)
 #'
 #' # create summary dataframe
 #' summary_nowcast(predictions)
 #'
 #'
 #' @export
-summary_nowcast <- function(nowcast_output, ...) {
+summary_nowcast <- function(nowcast_output) {
+  #set column names for summary
+  strata_unified_col <- ".strata_unified"
+  mean_col <- "mean"
+  sd_col <- "sd"
+  q05_col <- "q5"
+  q95_col <- "q95"
+  mean_col_new <- "Mean"
+  sd_col_new <- "SD"
+  q05_col_new <- "q05"
+  q95_col_new <- "q95"
   #Get names from input data
   onset_date_name <- nowcast_output$data$call_parameters$onset_date
-  strata_name <- nowcast_output$data$call_parameters$strata
+  #check if strata column in present
+  strata_name <- strata_name <- if (!is.null(nowcast_output$data$call_parameters$strata)) nowcast_output$data$call_parameters$strata else "Strata"
   date_dic <- nowcast_output$data$preprocessed_data |>
-    dplyr::select(.tval, onset_week) |>
+    dplyr::select(!!as.symbol(".tval"), dplyr::all_of(onset_date_name)) |>
     dplyr::distinct()
 
   predictions_summary <- nowcast_output$generated_quantities |>
@@ -33,30 +45,36 @@ summary_nowcast <- function(nowcast_output, ...) {
     posterior::subset_draws("N_predict") |>
     posterior::summarise_draws() |>
     # Extract strata and time values
-    dplyr::mutate(.strata = as.numeric(stringr::str_remove_all(variable, ".*\\[.*,|\\]")),
-                  .tval = as.numeric(stringr::str_remove_all(variable, ".*\\[|,.*\\]"))) |>
+    dplyr::mutate(.strata = as.numeric(stringr::str_remove_all(!!as.symbol("variable"), ".*\\[.*,|\\]")),
+                  .tval = as.numeric(stringr::str_remove_all(!!as.symbol("variable"), ".*\\[|,.*\\]"))) |>
     # assign the input strata names
     dplyr::left_join(nowcast_output$data$strata_dict, by = ".strata") |>
     # assign the onset dates
     dplyr::left_join(date_dic, by = ".tval") |>
     # Select and reorder the columns
-    dplyr::select(all_of(onset_date_name), .strata_unified, mean, sd, q5, q95)|>
-    # Rename the columns
+    dplyr::select(dplyr::all_of(onset_date_name), !!as.symbol(".strata_unified"),
+                  !!as.symbol("mean"), !!as.symbol("sd"), !!as.symbol("q5"), !!as.symbol("q95")) |>
+    # strata_name is a variable
+    dplyr::rename_with(~ strata_name, .cols = !!rlang::sym(strata_unified_col)) |>
+    # Specify a string for each remaining column
     dplyr::rename(
-      !!strata_name := .strata_unified,
-      Mean = mean,
-      SD = sd,
-      q05 = q5,
-      q95 = q95)
+      !!rlang::sym(mean_col_new) := !!rlang::sym(mean_col),
+      !!rlang::sym(sd_col_new) := !!rlang::sym(sd_col),
+      !!rlang::sym(q05_col_new) := !!rlang::sym(q05_col),
+      !!rlang::sym(q95_col_new) := !!rlang::sym(q95_col)
+    )
 
   return(predictions_summary)
 }
 
 
 
+
+
+
 #' Create plots form the nowcast() output
 #'
-#' Plots the prediction of the function nowcast() of the observed value
+#' Plots the predictions of the function nowcast() over the input observed values
 #'
 #' @param nowcast_output the output of the nowcast() function
 #'
@@ -69,75 +87,90 @@ summary_nowcast <- function(nowcast_output, ...) {
 #' now <- as.Date("1990-10-01")
 #'
 #' # perform nowcasting
-#' predictions <- nowcast(denguedat, "onset_week", "report_week", now = now, method = "optimization", seed = 2495624, iter = 10)
+#' predictions <- nowcast(denguedat, "onset_week", "report_week", now = now, method = "optimization",
+#' seed = 2495624, iter = 10)
 #'
 #' # create summary dataframe
 #' plot_nowcast(predictions)
 #'
 #'
 #' @export
-plot_nowcast <- function(nowcast_output, ...) {
+plot_nowcast <- function(nowcast_output) {
   # Get names from input data
   onset_date_name <- nowcast_output$data$call_parameters$onset_date
   strata_name <- nowcast_output$data$call_parameters$strata
 
+
   # Create dictionaries for onset week and strata
   date_dic <- nowcast_output$data$preprocessed_data |>
-    dplyr::select(.tval, onset_week) |>
+    dplyr::select(!!as.symbol(".tval"), !!dplyr::sym(onset_date_name)) |>
     dplyr::distinct()
   stata_dic <- nowcast_output$data$strata_dict
 
   # Make summary
   prediction_summary <- summary_nowcast(nowcast_output)
 
+  strata_name <- names(prediction_summary)[2]
+  #fixed colnames from summary
+  strata_unified_col <- ".strata_unified"
+  mean_col_new <- names(prediction_summary)[3]
+  sd_col_new <- names(prediction_summary)[4]
+  q05_col_new <- names(prediction_summary)[5]
+  q95_col_new <- names(prediction_summary)[6]
+
   # Get input data and sum over all delays
   data_delays <- nowcast_output$data$preprocessed_data |>
-    dplyr::group_by(.tval, .strata) |>
-    dplyr::summarise(n = sum(n), .groups = "drop") |>
+    dplyr::group_by(!!as.symbol(".tval"), !!as.symbol(".strata")) |>
+    dplyr::summarise(n = sum(!!as.symbol("n")), .groups = "drop") |>
     # Assign the onset dates
     dplyr::left_join(date_dic, by = ".tval") |>
     # Assign the strata names
     dplyr::left_join(stata_dic, by = ".strata") |>
     # Rename the strata column
-    dplyr::rename(!!strata_name := .strata_unified)
+    dplyr::rename_with(~ strata_name, .cols = !!rlang::sym(strata_unified_col))
+
 
   # Create plot with facets
-    ggplot2::ggplot(data_delays) +
+  plotnow <- ggplot2::ggplot(data_delays) +
     # Add prediction ribbon for 95% CI
     ggplot2::geom_ribbon(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), ymin = q05, ymax = q95, fill =  !!dplyr::sym(strata_name)),
-                         data = prediction_summary, alpha = 0.3
-      ) +
+      ggplot2::aes(x = !!dplyr::sym(onset_date_name), ymin = !!as.symbol("q05"), ymax = !!as.symbol("q95"), fill =  !!dplyr::sym(strata_name)),
+      data = prediction_summary, alpha = 0.3
+    ) +
     # Add real cases line
     ggplot2::geom_line(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), y = n, color = !!dplyr::sym(strata_name), linetype="Observed")
-      ) +
+      ggplot2::aes(x = !!dplyr::sym(onset_date_name), y = !!as.symbol("n"), color = !!dplyr::sym(strata_name), linetype="Observed")
+    ) +
     # Add predicted mean line
     ggplot2::geom_line(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), y = Mean, color = !!dplyr::sym(strata_name), linetype = "Predicted (Mean and 95% CI)"),
-                       data = prediction_summary
-      ) +
+      ggplot2::aes(x = !!dplyr::sym(onset_date_name), y = !!as.symbol("Mean"), color = !!dplyr::sym(strata_name),
+                   linetype = "Predicted (Mean and 95% CI)"),
+      data = prediction_summary
+    ) +
     # Facet by strata
-    ggplot2::facet_wrap(as.formula(paste("~", strata_name)), scales = "free_y") +
-      ggplot2::scale_linetype_manual("", values = c("Observed" = "solid", "Predicted (Mean and 95% CI)" = "dotted"))+
+    ggplot2::facet_wrap(stats::as.formula(paste("~", strata_name)), scales = "free_y") +
+    ggplot2::scale_linetype_manual("", values = c("Observed" = "solid", "Predicted (Mean and 95% CI)" = "dotted"))+
 
-      #Fixed legend for ribbon, mean, and real cases
-     #ggplot2::scale_color_discrete(name = NULL) +  # Keep colors mapped by strata
-     #ggplot2::scale_fill_discrete(name = NULL) #+   # Keep fills mapped by strata
+    # Manually control the legend
+    ggplot2::guides(
+      fill = "none",
+      color = "none"
+    ) +
 
-      # Manually control the legend
-      ggplot2::guides(
-        fill = "none",
-        color = "none"
-      ) +
+    # Labels and theme
+    ggplot2::labs(
+      y = "Cases"
+    ) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(
+      legend.position = "top",
+      legend.direction = "horizontal"
+    )
 
-      # Labels and theme
-      ggplot2::labs(
-        y = "Cases"
-      ) +
-      ggplot2::theme_minimal() +
-      ggplot2::theme(
-        legend.position = "top",
-        legend.direction = "horizontal"
-      )
+    # if all data as the same strata, it does not plot the title
+    if (length(unique(data_delays$.strata)) == 1) {
+      plotnow <- plotnow + ggplot2::theme(strip.text = ggplot2::element_blank())
+    }
+  return(plotnow)
 }
+
--- devel/summary_and_plot_matteo.R
+++ devel/summary_and_plot_matteo.R
@@ -6,8 +6,10 @@ devtools::load_all()
 #num_strata <- 5
 #num_delays <- 8
 #num_steps=15
-#sims  <- simulate_disease(num_steps=num_steps,num_strata = num_strata, num_delays = num_delays)
-#predictions_largelist <- nowcast(sims, "onset_week", "report_week", method="variational", strata = "gender")
+sims=simulate_disease(num_steps = 20, num_delays = 5, num_strata = 9)
+colnames(sims)[2] <- "imieistata"
+pred_sims = nowcast(sims, "onset_date", "report_date", method="variational",strata = "imieistata")
+plot_nowcast(pred_sims)
 
 denguedat_sel=denguedat[denguedat$onset_week >= as.Date("1991-09-01") & denguedat$onset_week <= as.Date("1991-12-01"),]
 set.seed(122)  # Set seed for reproducibility
@@ -23,7 +25,7 @@ denguedat_sel$gender[indices] <- "uncertain"
 
 
 
-predictions_largelist <-
+predictions_dengue<-
   nowcast(denguedat_sel, "onset_week", "report_week", method="variational",
           strata = "gender")
 
@@ -37,105 +39,11 @@ predictions_largelist <-
 
 
 
-summary_nowcast <- function(nowcast_output, ...) {
-  #Get names from input data
-  onset_date_name <- nowcast_output$data$call_parameters$onset_date
-  strata_name <- nowcast_output$data$call_parameters$strata
-  date_dic <- nowcast_output$data$preprocessed_data |>
-    dplyr::select(.tval, onset_week) |>
-    dplyr::distinct()
-
-  predictions_summary <- nowcast_output$generated_quantities |>
-    posterior::as_draws() |>
-    posterior::subset_draws("N_predict") |>
-    posterior::summarise_draws() |>
-    # Extract strata and time values
-    dplyr::mutate(.strata = as.numeric(stringr::str_remove_all(variable, ".*\\[.*,|\\]")),
-                  .tval = as.numeric(stringr::str_remove_all(variable, ".*\\[|,.*\\]"))) |>
-    # assign the input strata names
-    dplyr::left_join(nowcast_output$data$strata_dict, by = ".strata") |>
-    # assign the onset dates
-    dplyr::left_join(date_dic, by = ".tval") |>
-    # Select and reorder the columns
-    dplyr::select(all_of(onset_date_name), .strata_unified, mean, sd, q5, q95)|>
-    # Rename the columns
-    dplyr::rename(
-      !!strata_name := .strata_unified,
-      Mean = mean,
-      SD = sd,
-      q05 = q5,
-      q95 = q95)
-
-  return(predictions_summary)
-}
 
 
 
 
-plot_nowcast <- function(nowcast_output, ...) {
-  # Get names from input data
-  onset_date_name <- nowcast_output$data$call_parameters$onset_date
-  strata_name <- nowcast_output$data$call_parameters$strata
 
-  # Create dictionaries for onset week and strata
-  date_dic <- nowcast_output$data$preprocessed_data |>
-    dplyr::select(.tval, onset_week) |>
-    dplyr::distinct()
-  stata_dic <- nowcast_output$data$strata_dict
 
-  # Make summary
-  prediction_summary <- summary_nowcast(nowcast_output)
+###########PLOT
 
-  # Get input data and sum over all delays
-  data_delays <- nowcast_output$data$preprocessed_data |>
-    dplyr::group_by(.tval, .strata) |>
-    dplyr::summarise(n = sum(n), .groups = "drop") |>
-    # Assign the onset dates
-    dplyr::left_join(date_dic, by = ".tval") |>
-    # Assign the strata names
-    dplyr::left_join(stata_dic, by = ".strata") |>
-    # Rename the strata column
-    dplyr::rename(!!strata_name := .strata_unified)
-
-  # Create plot with facets
-    ggplot2::ggplot(data_delays) +
-    # Add prediction ribbon for 95% CI
-    ggplot2::geom_ribbon(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), ymin = q05, ymax = q95, fill =  !!dplyr::sym(strata_name)),
-                         data = prediction_summary, alpha = 0.3
-      ) +
-    # Add real cases line
-    ggplot2::geom_line(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), y = n, color = !!dplyr::sym(strata_name), linetype="Observed")
-      ) +
-    # Add predicted mean line
-    ggplot2::geom_line(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), y = Mean, color = !!dplyr::sym(strata_name), linetype = "Predicted (Mean and 95% CI)"),
-                       data = prediction_summary
-      ) +
-    # Facet by strata
-    ggplot2::facet_wrap(as.formula(paste("~", strata_name)), scales = "free_y") +
-      ggplot2::scale_linetype_manual("", values = c("Observed" = "solid", "Predicted (Mean and 95% CI)" = "dotted"))+
-
-      #Fixed legend for ribbon, mean, and real cases
-     #ggplot2::scale_color_discrete(name = NULL) +  # Keep colors mapped by strata
-     #ggplot2::scale_fill_discrete(name = NULL) #+   # Keep fills mapped by strata
-
-      # Manually control the legend
-      ggplot2::guides(
-        fill = "none",
-        color = "none"
-      ) +
-
-      # Labels and theme
-      ggplot2::labs(
-        y = "Cases"
-      ) +
-      ggplot2::theme_minimal() +
-      ggplot2::theme(
-        legend.position = "top",
-        legend.direction = "horizontal"
-      )
-}
-
-plot_nowcast(predictions_largelist)
