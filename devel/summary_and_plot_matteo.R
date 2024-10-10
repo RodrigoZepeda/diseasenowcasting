03645d40c78a8d5b8814c08042fd3b9b5a4063fb
Matteo Perini <perini.mtt@gmail.com>
2024-10-10 18:03:07 -0400

    dev on new summary and plot

--- devel/summary_and_plot_matteo.R
+++ devel/summary_and_plot_matteo.R
@@ -1,3 +1,11 @@
+
+#### check with no strata, check with . in the strata name
+
+
+
+
+
+
 #rstantools::rstan_config()
 devtools::load_all()
 
@@ -13,7 +21,7 @@ plot_nowcast(pred_sims)
 
 denguedat_sel=denguedat[denguedat$onset_week >= as.Date("1991-09-01") & denguedat$onset_week <= as.Date("1991-12-01"),]
 set.seed(122)  # Set seed for reproducibility
-n <- nrow(denguedat_sel)  # Total number of rowsß
+n <- nrow(denguedat_sel)  # Total number of rows
 indices <- sample(1:n, size = ceiling(n / 4))  # Randomly select a third of the rows
 # Replace the selected indices in the gender column with "other"
 denguedat_sel$gender[indices] <- "Other"
@@ -46,28 +54,19 @@ predictions_dengue<-
 #' # @return A a ggplot2 object that plots the evolution of the nowcast over the observed values,
 #' it creates a subplot for each strata
 
-summary_nowcast_dev <- function(nowcast_output) {
-  # set column names for summary                   #### avoid renaming the unnecessary. a bunch of quantiles and stratas.
-  strata_unified_col <- ".strata_unified"
-  mean_col <- "mean"
-  sd_col <- "sd"       ## need to go, must be handled by posterior::summarise_draw
-  q05_col <- "q5"
-  q95_col <- "q95"
-  mean_col_new <- "Mean"
-  sd_col_new <- "SD"
-  q05_col_new <- "q05"
-  q95_col_new <- "q95"
+summary_nowcast_dev <- function(nowcast_output, quantiles=NULL) {
+
+
+  quants <- if (is.null(quantiles)) c(0.05, 0.95) else quantiles
+  # Check if quants is a numeric vector with values between 0 and 1
+  if (!is.numeric(quants) || any(quants < 0 | quants > 1)) {
+    stop("quants must be a numeric vector with values between 0 and 1.")
+  }
+
+
   # Get names from input data
   onset_date_name <- nowcast_output$data$call_parameters$onset_date
-  # check if strata column in present
-  # Get strata names if present
-  if (is.null(nowcast_output$data$call_parameters$strata)) {
-    strata_names <- "Strata"
-  } else if (length(nowcast_output$data$call_parameters$strata)==1) {
-    strata_names <- nowcast_output$data$call_parameters$strata
-  } else {
-    strata_names <- nowcast_output$data$call_parameters$strata
-  }
+
 
   date_dic <- nowcast_output$data$preprocessed_data |>
     dplyr::select(!!as.symbol(".tval"), dplyr::all_of(onset_date_name)) |>
@@ -76,7 +75,7 @@ summary_nowcast_dev <- function(nowcast_output) {
   predictions_summary <- nowcast_output$generated_quantities |>
     posterior::as_draws() |>
     posterior::subset_draws("N_predict") |>
-    posterior::summarise_draws("mean","sd",~quantile(.x, probs = c(0.4, 0.6))) |> #just need to parametrize this
+    posterior::summarise_draws("mean","sd",~quantile(.x, probs = quants)) |>
     # Extract strata and time values
     dplyr::mutate(
       .strata = as.numeric(stringr::str_remove_all(!!as.symbol("variable"), ".*\\[.*,|\\]")),
@@ -88,29 +87,37 @@ summary_nowcast_dev <- function(nowcast_output) {
     dplyr::left_join(date_dic, by = ".tval") |>
     # Select and reorder the columns
     dplyr::select(
-      dplyr::all_of(onset_date_name), !!as.symbol(".strata_unified"),
-      !!as.symbol("mean"), !!as.symbol("sd"), !!as.symbol("q5"), !!as.symbol("q95")
+      !!rlang::sym(onset_date_name), everything(), -!!as.symbol("variable"),  -!!as.symbol(".tval"), -!!as.symbol(".strata"),
     ) |>
-    # strata_name is a variable
-    dplyr::rename_with(~strata_name, .cols = !!rlang::sym(strata_unified_col)) |>  ### this needs to go
-    # Specify a string for each remaining column                            ### this needs to go
-    dplyr::rename(
-      !!rlang::sym(mean_col_new) := !!rlang::sym(mean_col),
-      !!rlang::sym(sd_col_new) := !!rlang::sym(sd_col),
-      !!rlang::sym(q05_col_new) := !!rlang::sym(q05_col),
-      !!rlang::sym(q95_col_new) := !!rlang::sym(q95_col)
-    )
+    dplyr::rename(!!as.symbol("Strata_unified") := !!as.symbol(".strata_unified"))
 
   return(predictions_summary)
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 plot_nowcast_dev <- function(nowcast_output) {
   # Get names from input data
   onset_date_name <- nowcast_output$data$call_parameters$onset_date
-  strata_name <- nowcast_output$data$call_parameters$strata
-
-
+  # Get strata names if present
+  #strata_names <- nowcast_output$data$call_parameters$strata
+  Strata_unified = "Strata_unified"
+  dot_strata_unified = ".strata_unified"
   # Create dictionaries for onset week and strata
   date_dic <- nowcast_output$data$preprocessed_data |>
     dplyr::select(!!as.symbol(".tval"), !!dplyr::sym(onset_date_name)) |>
@@ -118,15 +125,16 @@ plot_nowcast_dev <- function(nowcast_output) {
   stata_dic <- nowcast_output$data$strata_dict
 
   # Make summary
-  prediction_summary_dev <- summary_nowcast_dev(nowcast_output)
+  prediction_summary <- summary_nowcast_dev(nowcast_output) ###### good only during dev !!!!!!!##$##%$##$
+  #prediction_summary <- summary_nowcast(nowcast_output)
 
-  strata_name <- names(prediction_summary)[2]
+  #strata_name <- names(prediction_summary)[2]
   # fixed colnames from summary
-  strata_unified_col <- ".strata_unified"
-  mean_col_new <- names(prediction_summary)[3]
-  sd_col_new <- names(prediction_summary)[4]
-  q05_col_new <- names(prediction_summary)[5]
-  q95_col_new <- names(prediction_summary)[6]
+  #strata_unified_col <- ".strata_unified"
+  #mean_col_new <- names(prediction_summary)[3]
+  #sd_col_new <- names(prediction_summary)[4]
+  #q05_col_new <- names(prediction_summary)[5]
+  #q95_col_new <- names(prediction_summary)[6]
 
   # Get input data and sum over all delays
   data_delays <- nowcast_output$data$preprocessed_data |>
@@ -137,30 +145,31 @@ plot_nowcast_dev <- function(nowcast_output) {
     # Assign the strata names
     dplyr::left_join(stata_dic, by = ".strata") |>
     # Rename the strata column
-    dplyr::rename_with(~strata_name, .cols = !!rlang::sym(strata_unified_col))
+    dplyr::rename_with(~Strata_unified, .cols = !!rlang::sym(dot_strata_unified))
 
 
   # Create plot with facets
   plotnow <- ggplot2::ggplot(data_delays) +
     # Add prediction ribbon for 95% CI
     ggplot2::geom_ribbon(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), ymin = !!as.symbol("q05"), ymax = !!as.symbol("q95"), fill = !!dplyr::sym(strata_name)),
+      ggplot2::aes(x = !!dplyr::sym(onset_date_name), ymin = !!as.symbol("5%"), ymax = !!as.symbol("95%"), fill = !!as.symbol("Strata_unified")),
       data = prediction_summary, alpha = 0.3
     ) +
     # Add real cases line
     ggplot2::geom_line(
-      ggplot2::aes(x = !!dplyr::sym(onset_date_name), y = !!as.symbol("n"), color = !!dplyr::sym(strata_name), linetype = "Observed")
-    ) +
+      ggplot2::aes(
+        x = !!dplyr::sym(onset_date_name), y = !!as.symbol("n"), color = !!as.symbol("Strata_unified"),
+        linetype = "Observed")
+      ) +
     # Add predicted mean line
     ggplot2::geom_line(
       ggplot2::aes(
-        x = !!dplyr::sym(onset_date_name), y = !!as.symbol("Mean"), color = !!dplyr::sym(strata_name),
+        x = !!dplyr::sym(onset_date_name), y = !!as.symbol("mean"), color = !!as.symbol("Strata_unified"),
         linetype = "Predicted (Mean and 95% CI)"
-      ),
-      data = prediction_summary
+      ),data = prediction_summary
     ) +
     # Facet by strata
-    ggplot2::facet_wrap(stats::as.formula(paste("~", strata_name)), scales = "free_y") +
+    ggplot2::facet_wrap(stats::as.formula("~ Strata_unified"), scales = "free_y") + ##########miss !!as.symbol or smth
     ggplot2::scale_linetype_manual("", values = c("Observed" = "solid", "Predicted (Mean and 95% CI)" = "dotted")) +
 
     # Manually control the legend
@@ -189,8 +198,4 @@ plot_nowcast_dev <- function(nowcast_output) {
 
 
 
-
-
-
-###########PLOT
 
